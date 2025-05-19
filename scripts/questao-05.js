2b337fa50d58b7017992392c939098ed69645128
Jhonatan <jhonatan.favoreto18@gmail>
2025-05-19 11:20:34 -0300

    feat: Finaliza resolução da questão 05.

---
+++ scripts/questao-05.js
@@ -0,0 +1,18 @@
+const precos = [150, 50, 220, 80, 120, 30, 480, 70, 90, 190];
+const precosComDesconto = [];
+let contadorArrayPrecoDesconto = 0;
+let preco = 0;
+let desconto = 0;
+
+//Varredura de array com laço de repetição
+for (let i = 0; i < precos.length; i++) {
+    preco = precos[i];
+    if (precos[i] > 100) {
+        desconto = precos[i] * 0.1; // 10/100 
+        preco = precos[i] - desconto;
+    }
+    precosComDesconto[contadorArrayPrecoDesconto] = preco;
+    contadorArrayPrecoDesconto++;
+} 
+console.table(precos);
+console.table(precosComDesconto);
