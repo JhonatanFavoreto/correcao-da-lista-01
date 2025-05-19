5756cc9f84d26aebe5845c42f883a453cde07cb9
Jhonatan <jhonatan.favoreto18@gmail>
2025-05-19 09:41:59 -0300

    feat: Finaliza resolução da questão 02.

---
+++ scripts/questao-02.js
@@ -0,0 +1,23 @@
+const numeros = [12, 5, 8, 21, 16, 7, 30, 45, 13, 27];
+let numerosPares = 0
+let numerosImpares = 0
+let maioresQue20 = 0
+
+//Varredura do array.
+for (let i = 0; i < numeros.length; i++) {
+    //Codicional para números pares.
+    if (numeros[i] %2 == 0) {
+        numerosPares++;
+    } else {
+        numerosImpares++;
+    }
+
+    //Codicional para maiores que 20
+    if (numeros[i] > 20) {
+        maioresQue20++;
+    }
+}
+
+console.log(`A quantidade de números pares é: ${numerosImpares}.`)
+console.log(`A quantidade de números impares é: ${numerosPares}.`)
+console.log(`A quantidade de números que 20 é: ${maioresQue20}.`)
